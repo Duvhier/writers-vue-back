03a2d7ebf559f20ccd0daac3c5bf3c61f352a76e
Duvier <duvier.tavera01@unicatolica.edu.co>
2025-05-21 09:10:41 -0500

    WIP: cambios en Writer.service.ts

--- src/services/Writer.service.ts
+++ src/services/Writer.service.ts
@@ -2,10 +2,10 @@ import { WriterRepo } from "../repositories/Writer.repository";
 import { Writer } from "../entities/Writer.entity";
 export class WriterService {
     async createWriter(data: Partial<Writer>): Promise<Writer> {
-        const w = WriterRepo.create(data);        // Construye la entidad 
-        return WriterRepo.save(w);                // Persiste en BD 
+        const w = WriterRepo.create(data);        
+        return WriterRepo.save(w);                
     }
     async getAllWriters(): Promise<Writer[]> {
-        return WriterRepo.find({ relations: ["books"] }); // Incluye libros 
+        return WriterRepo.find({ relations: ["books"] }); 
     }
 } 
